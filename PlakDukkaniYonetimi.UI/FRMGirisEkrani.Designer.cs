c0e5a4cf6d932ab4431b49e0f7516165b6e92b4c
gokayoz <gokay.oz@outlook.com.tr>
2025-05-30 23:54:44 +0300

    Updates FRMGirisEkrani form

--- PlakDukkaniYonetimi.UI/FRMGirisEkrani.Designer.cs
+++ PlakDukkaniYonetimi.UI/FRMGirisEkrani.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // FRMGirisEkrani
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(46, 46, 46);
+            ClientSize = new Size(346, 450);
+            ForeColor = Color.WhiteSmoke;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FRMGirisEkrani";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
         }
 
         #endregion
